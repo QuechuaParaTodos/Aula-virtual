98fe76da2d42d2e53de2d9a1a60a7d3b12578908
Dennis Huillca <dennisbot@gmail.com>
2021-09-29 00:13:19 -0500

    Add Not Found page.

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,14 +1,24 @@
 <template>
-  <v-app dark>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
-    </h1>
-    <NuxtLink to="/">
-      Home page
-    </NuxtLink>
+  <v-app>
+    <v-main class="error-page">
+      <v-divider class="hr-line"></v-divider>
+      <div class="error-wrapper">
+        <div class="apu-container">
+          <h1>{{ apuBigTextMessage }}</h1><img src="~assets/ApuTriste.png" alt="apu triste">
+        </div>
+        <div class="subtext-container">
+          <h1 v-if="error.statusCode === 404">
+            {{ pageNotFound }}
+          </h1>
+          <h1 v-else>
+            {{ otherError }}
+          </h1>
+          <NuxtLink to="/">
+            Regresar
+          </NuxtLink>
+        </div>
+      </div>
+    </v-main>
   </v-app>
 </template>
 
@@ -23,7 +33,8 @@ export default {
   },
   data () {
     return {
-      pageNotFound: '404 Not Found',
+      apuBigTextMessage: 'Oops!',
+      pageNotFound: 'No encontramos la página que buscas :(',
       otherError: 'An error occurred'
     }
   },
@@ -38,7 +49,56 @@ export default {
 </script>
 
 <style scoped>
-h1 {
-  font-size: 20px;
+.error-page {
+  background-color: #FFFEFD;
+}
+
+.hr-line {
+  border: 2px solid #ccc;
+  left: 0;
+  position: absolute;
+  top: 60%;
+  width: 100%;
+}
+
+.error-wrapper {
+  left: 50%;
+  position: relative;
+  text-align: center;
+  top: calc(60% - 270px);
+  transform: translate(-50%);
+}
+
+.apu-container h1 {
+  color: #939393;
+  display: inline-block;
+  font-size: 100px;
+  font-weight: bold;
+}
+
+.apu-container img {
+  margin-bottom: -40px;
+}
+
+.subtext-container {
+  margin-left: 70px;
+  margin-top: -10px;
+}
+
+.subtext-container h1 {
+  color: #393939;
+  font-size: 36px;
+}
+
+.subtext-container a {
+  color: #BA77CA;
+  display: inline-block;
+  font-size: 28px;
+  font-weight: bold;
+  margin-top: 9px;
+  text-decoration: none;
+}
+.subtext-container a:hover {
+  color: #ad5fc0;
 }
 </style>
