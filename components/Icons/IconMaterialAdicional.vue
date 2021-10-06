84283218b1263c2531158c5ac25a7969dadf92ef
AliceRamirez17 <alicemarcelaramirez@gmail.com>
2021-10-05 20:31:38 -0500

    fix: Icons Curso SidebarLeft fixed

---
+++ components/Icons/IconMaterialAdicional.vue
@@ -0,0 +1,21 @@
+<template>
+    <svg
+        width="40"
+        height="40"
+        viewBox="0 0 40 40"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <circle cx="20" cy="20" r="19.25" stroke="#4AA0AF" stroke-width="1.5" />
+        <path
+            d="M8 19.9969H16.0016V12H8V19.9969ZM17.9984 12.9984V15H32V12.9984H17.9984ZM17.9984 18.9984H32V16.9969H17.9984V18.9984ZM17.9984 24.9984H32V22.9969H17.9984V24.9984ZM17.9984 28.9969H32V26.9953H17.9984V28.9969ZM8 30H16.0016V21.9984H8V30Z"
+            fill="currentColor"
+        />
+    </svg>
+</template>
+
+<script>
+export default {
+    name: "IconMaterialAdicional"
+};
+</script>
