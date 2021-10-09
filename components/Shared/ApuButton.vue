55a950673a3182f0a1d96f42e2ad52b63a8c4939
Dennis Huillca <dennisbot@gmail.com>
2021-10-08 21:08:04 -0500

    Improve ApuButton custom component.
    I followed this recommendations:
    https://dev.to/onurelibol/creating-custom-components-with-vuetify-inheriting-props-events-slots-in-composition-api-56ef

--- components/Shared/ApuButton.vue
+++ components/Shared/ApuButton.vue
@@ -1,32 +1,39 @@
 <template>
-    <div class="btn-container chakana-shape">
-        <button @click="onClick" plain outlined class="btn-chakana chakana-shape">
-            <slot></slot>
-        </button>
-    </div>
+    <v-btn v-bind="$attrs" v-on="$listeners" class="btn-chakana chakana-shape">
+        <slot />
+    </v-btn>
 </template>
 
 <script>
 export default {
-    inheritAttrs: true,
+    inheritAttrs: false,
+    mounted() {
+        this.wrap(this.$el, this.$options._scopeId);
+    },
     methods: {
-        onClick() {
-            this.$emit("click");
+        wrap(toWrap, scopeId) {
+            const wrapper = document.createElement("div");
+            if (scopeId) wrapper.toggleAttribute(scopeId);
+            wrapper.classList.add("btn-container", "chakana-shape");
+            toWrap.parentNode.appendChild(wrapper);
+            wrapper.appendChild(toWrap);
         }
     }
 };
 </script>
 
 <style lang="scss" scoped>
-$chakana-style-color: #e73324 !important;
 $btn-border-color: $chakana-style-color;
 $btn-text-color: $chakana-style-color;
 $btn-hover-background-color: $chakana-style-color;
 
-$chakana-alt-style-color: white !important;
 $btn-background-color: $chakana-alt-style-color;
 $btn-hover-text-color: $chakana-alt-style-color;
-$edge-width: 6px;
+$vertex-width: 6px;
+
+::v-deep .v-btn__loader {
+    color: $chakana-alt-style-color;
+}
 
 .btn-container {
     background: $btn-border-color;
@@ -39,7 +46,8 @@ $edge-width: 6px;
     border: none;
     color: $btn-text-color;
     font-weight: bold;
-    padding: 4px 50px;
+    height: 32px !important;
+    text-transform: unset;
 
     &:hover {
         background: $btn-hover-background-color;
@@ -49,18 +57,18 @@ $edge-width: 6px;
 
 .chakana-shape {
     clip-path: polygon(
-        0 $edge-width,
-        $edge-width $edge-width,
-        $edge-width 0,
-        calc(100% - #{$edge-width}) 0,
-        calc(100% - #{$edge-width}) $edge-width,
-        100% $edge-width,
-        100% calc(100% - #{$edge-width}),
-        calc(100% - #{$edge-width}) calc(100% - #{$edge-width}),
-        calc(100% - #{$edge-width}) 100%,
-        $edge-width 100%,
-        $edge-width calc(100% - #{$edge-width}),
-        0 calc(100% - #{$edge-width})
+        0 $vertex-width,
+        $vertex-width $vertex-width,
+        $vertex-width 0,
+        calc(100% - #{$vertex-width}) 0,
+        calc(100% - #{$vertex-width}) $vertex-width,
+        100% $vertex-width,
+        100% calc(100% - #{$vertex-width}),
+        calc(100% - #{$vertex-width}) calc(100% - #{$vertex-width}),
+        calc(100% - #{$vertex-width}) 100%,
+        $vertex-width 100%,
+        $vertex-width calc(100% - #{$vertex-width}),
+        0 calc(100% - #{$vertex-width})
     );
 }
 </style>
--- pages/Home/index.vue
+++ pages/Home/index.vue
@@ -1,23 +1,42 @@
 <template>
     <div>
         home
-        <p><ApuButton @click="showAlert">iniciar sesión</ApuButton></p>
+        <p>
+            <ApuButton
+                @click="showAlert"
+                :loading="loading"
+                :disabled="loading"
+                :ripple="false"
+                class="py-1 px-10"
+            >
+                Iniciar Sesión
+            </ApuButton>
+        </p>
     </div>
 </template>
 
 <script>
-    import ApuButton from '../../components/Shared/ApuButton.vue'
-    export default {
-        components: {
-            ApuButton
-        },
-        methods: {
-            showAlert: () => {
-                alert('mensaje de prueba');
-            }
+import ApuButton from "~/components/Shared/ApuButton.vue";
+export default {
+    components: {
+        ApuButton
+    },
+    data() {
+        return {
+            loading: false
+        };
+    },
+    methods: {
+        showAlert(e) {
+            console.log("e", e);
+            // alert("mensaje de prueba");
+            this.loading = true;
+            setTimeout(() => {
+                this.loading = false;
+            }, 2000);
         }
     }
+};
 </script>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
