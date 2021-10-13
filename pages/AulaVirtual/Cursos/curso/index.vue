2abb32bfe6e881a3034b94da70793f6fb71ed4ed
carmen.farinez <acuario1140@gmail.com>
2021-10-12 23:12:10 -0500

    add component videoEmbed

--- pages/AulaVirtual/Cursos/curso/index.vue
+++ pages/AulaVirtual/Cursos/curso/index.vue
@@ -1,11 +1,14 @@
 <template>
     <v-container fluid>
         <h1>Aula virtual - Mis Cursos - Curso Quechua</h1>
+       
     </v-container>
 </template>
 
 <script>
+
 export default {
     layout: "aulavirtual"
+   
 };
 </script>
--- pages/AulaVirtual/Cursos/index.vue
+++ pages/AulaVirtual/Cursos/index.vue
@@ -1,14 +1,30 @@
 <template>
-    <v-container fluid>
-        <h1>Aula virtual - Home</h1>
-    </v-container>
+    <v-row>
+        <div>
+            <Sidebarleft />
+        </div>
+        <v-col class="v-sheet v-sheet--outlined theme--light mt-4">
+                <VideoEmbed class="mx-16"/>
+                <Questions class="mt-5"/>
+        </v-col>
+    </v-row>
 </template>
 
 <script>
+import Questions from "@/components/Questions/Questions";
+import Sidebarleft from "@/components/AulaVirtual/Sidebarleft";
+import Breadcrumbs from "@/components/Breadcrumbs";
+import VideoEmbed from "@/components/VideoEmbed";
 export default {
+    components: {Questions, Breadcrumbs, Sidebarleft, VideoEmbed},
     head: {
         title: "Aula Virtual"
     },
     layout: "aulavirtual"
 };
 </script>
+
+<style>
+
+
+</style>
